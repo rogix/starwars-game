dc771efc01ee90f4d05aca09911007427cd95502
Roger <12768403+rogix@users.noreply.github.com>
2019-07-17 14:46:49 -0300

    Updates

---
+++ precache-manifest.b78fc5162b170f9a6400820959533755.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afaec875206b34e78475608b92893e31",
+    "url": "/starwars-game/index.html"
+  },
+  {
+    "revision": "0a372524ad52ae071943",
+    "url": "/starwars-game/static/css/main.411a2b16.chunk.css"
+  },
+  {
+    "revision": "3cc7f05352d8eadd290c",
+    "url": "/starwars-game/static/js/2.6e87735a.chunk.js"
+  },
+  {
+    "revision": "0a372524ad52ae071943",
+    "url": "/starwars-game/static/js/main.7205cf06.chunk.js"
+  },
+  {
+    "revision": "0b57fb61aac7f03f1656",
+    "url": "/starwars-game/static/js/runtime~main.52bcb9b9.js"
+  }
+]);
